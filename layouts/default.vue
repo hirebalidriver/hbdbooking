0879fab901c9e469b675f19b6c241a0f228ec8d0
Andre Nata <andrenata25@gmail.com>
2023-04-19 22:12:32 +0800

    first

---
+++ layouts/default.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { onMounted } from "vue";
+import { initFlowbite } from "flowbite";
+import StepperView from "../components/StepperView.vue";
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+  initFlowbite();
+});
+</script>
+
+<template>
+  <div class="container mx-auto px-6">
+    <div class="py-5 flex justify-between">
+      <div class="w-32">
+        <img src="@/static/image/logo-hbd.png" alt="Hire Bali Driver" />
+      </div>
+      <nuxt-link to="https://hirebalidriver.com">Home</nuxt-link>
+    </div>
+
+    <div class="pt-5 pb-5 w-full md:w-48">
+      <stepper-view></stepper-view>
+    </div>
+    <div>
+      <div
+        class="w-full p-6 bg-white border border-gray-200 rounded-lg shadow-lg"
+      >
+        <Nuxt />
+      </div>
+    </div>
+  </div>
+</template>
