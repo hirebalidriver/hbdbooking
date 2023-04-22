a7a38b4f74355df028595e208edfd58459c0f7a3
Andre Nata <andrenata25@gmail.com>
2023-04-22 23:04:08 +0800

    add: notfound

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,20 @@
 <template>
   <div>
-    <div class="grid grid-cols-1 mt-5 md:gap-4 md:grid-cols-3">
-      <div class="col-span-2">
-        <product-order></product-order>
+    <div class="min-h-screen hero bg-base-200">
+      <div class="text-center hero-content">
+        <div class="max-w-md">
+          <h1 class="mb-5 text-5xl font-bold">Not Found</h1>
+          <a href="https://hirebalidriver.com" class="lowercase btn btn-success"
+            >www.hirebalidriver.com</a
+          >
+        </div>
       </div>
-      <div class="w-full mt-4 md:mt-0"><form-calendar></form-calendar></div>
-    </div>
-    <div class="grid gap-4 mt-5">
-      <options-view title="Besakih - All inclusive" price="89"></options-view>
-      <options-view title="Besakih without Lunch" price="79"></options-view>
     </div>
   </div>
 </template>
 
 <script>
-import FormCalendar from "../components/FormCalendar.vue";
-import OptionsView from "../components/OptionsView.vue";
-import ProductOrder from "../components/ProductOrder.vue";
 export default {
-  components: { FormCalendar, ProductOrder, OptionsView },
-  layout: "default",
+  layout: "notfound",
 };
 </script>
