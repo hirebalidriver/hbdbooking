fecba1b4d4964f8cc574361b101e0d9d819f3ec4
Andre Nata <andrenata25@gmail.com>
2023-05-15 21:24:23 +0800

    Meta and favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,17 +1,25 @@
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "hbdbooking",
+    title: "Most Popular Tours in Bali | Hire Bali Driver",
     htmlAttrs: {
       lang: "en",
     },
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
+      {
+        hid: "description",
+        name: "description",
+        content: "Hire Bali Driver",
+      },
+      {
+        hid: "robots",
+        name: "robots",
+        content: "noindex, follow",
+      },
       { name: "format-detection", content: "telephone=no" },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }],
   },
 
   ssr: true,
