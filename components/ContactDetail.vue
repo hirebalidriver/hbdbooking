c9d209a35ccdcb2c264a1bdf4cf5777e0f80ec08
kapamungkas <kap21kap@gmail.com>
2024-11-11 08:54:03 +0800

    Validate email

--- components/ContactDetail.vue
+++ components/ContactDetail.vue
@@ -42,7 +42,9 @@
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                 v-model="form.email"
                 required
+                @input="validateEmail"
               />
+              <p v-if="emailError" class="text-red-500 text-sm">{{ emailError }}</p>
             </div>
             <div class="mb-3">
               <label class="block mb-2 text-sm font-medium text-gray-900"
@@ -502,6 +504,17 @@ export default {
         .render(this.$refs.paypal);
     },
     // END PAYPAL
+
+    // validasi
+    validateEmail() {
+      // Regex sederhana untuk validasi email
+      const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      if (this.form.email && !emailPattern.test(this.form.email)) {
+        this.emailError = "Email is not valid";
+      } else {
+        this.emailError = "";
+      }
+    },
   },
 };
 </script>
