2024857d568058d5d296624654bdc640c53d1012
kapamungkas <kap21kap@gmail.com>
2024-11-12 13:44:38 +0800

    up bold email

--- pages/success/_id.vue
+++ pages/success/_id.vue
@@ -9,7 +9,7 @@
             Reservation success
           </h1>
           <h2 class="mb-10">
-            Booking details has been sent to {{ booking.email }}
+            Booking details has been sent to <b>{{ booking.email }}</b>
           </h2>
         </div>
         <div class="mb-2 font-semibold">
